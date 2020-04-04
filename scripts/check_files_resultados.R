7f77c75855c91c3e034c8c3102fcd302d75c8175
TuQmano-Lenovo <juanpabloruiznicolini@gmail.com>
2020-04-04 15:06:00 -0300

    Edito script para chequear files resultados
    consistencia de nombres y cantidad de elecciones por distrito

--- scripts/check_files_resultados.R
+++ scripts/check_files_resultados.R
@@ -1,5 +1,5 @@
 #### Bases de datos de elecciones nacionales de Argetina
-#### Lectura de archivos desde Google Drive
+#### Lectura de archivos desde Google Drive - juanpabloruiznicolini@gmail.com
 
 # Cargo libererias
 library(tidyverse)
@@ -7,43 +7,37 @@ library(googledrive)  # va a pedir autentica acceso a cuenta de GMAIL vía API d
 
 
 # RUTA DONDE ESTAN LOS DATOS
-datos <- "https://drive.google.com/open?id=1LzqbqS2rmMthzYpZYhPU3R8umaJZOt_U"
+ruta <- "https://drive.google.com/open?id=1LzqbqS2rmMthzYpZYhPU3R8umaJZOt_U"
 
 
 # CARGO EL LISTADO DE DATOS DE LA CARPETA
-datos <- googledrive::drive_ls(path = datos,
+datos <- googledrive::drive_ls(path = ruta,
                                recursive = T) # Recursive trae los archivos de todas las careptas
   
 # Genero una nueva tabla descomponiendo nombre de arhcivos en parametros
 
-archivos <- datos[1] %>%
+archivos <- datos %>% 
   arrange(name) %>% 
-  slice(26:458) %>%                     # LIMPIO NOMBRES DE CARPETAS   
+  slice(26:464) %>%                     # LIMPIO NOMBRES DE CARPETAS   
+  group_by(name) %>% 
+  slice(1) %>%  # sjuan_sen_paso2017 esta duplicado. No encuentro bug. En la base una sola vez y tiene = id
+  ungroup() %>% 
   filter(!str_detect(name, "2019"),     # EXCLUYO ELECCION 2019 (mal naming y otro formato)
          !str_detect(name, "presi")) %>%  # Excluyo  presi / otro formato
   mutate(name = str_to_lower(name)) %>% 
   separate(name, into = c("provincia", "categoria", "turno"), 
-           sep = "\\_", remove = F) %>% 
-  separate(turno,"turno", sep = "\\.")
+           sep = "[:punct:]", remove = F) 
 
 ### CHEQUEO CONSISTENCIA EN CANTIDAD DE ARCHIVOS 
 archivos %>% 
   group_by(turno) %>%  # DEBERIAN SER 32 (24 Diputados + 8 Senadores) x turno
   summarise(n = n())
 
-
-archivos %>%    # Chequo de n(elecciones) x provincia para corregir archivos de origen 
-  group_by(provincia) %>% 
+# Chequo de n(elecciones) x provincia y categoria
+archivos %>%    
+  group_by(provincia, categoria) %>%  # 8 provincias tienen una eleccion mas de senador (turno 2009)
   summarise(n = n()) %>% 
   arrange(n,provincia) %>% 
   print(n = Inf)
-
-
-
-
-
-
-
-
 
 
